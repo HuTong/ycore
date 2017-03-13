2fa567b18ea6e3ba266f5a861a8f80a2029dd3bc
hutong <hutong0521@163.com>
2017-03-13 23:12:09 +0800

    修复

--- Xmodel.php
+++ Xmodel.php
@@ -1,9 +1,24 @@
 <?php
 namespace HuTong\Ycore;
+use HuTong\Database\Database as Database;
 /**
  * Model 基类
  */
 class Xmodel
 {
 	protected $db;
+
+	public function __construct($dbName = 'default')
+	{
+		if(is_null($this->db))
+		{
+			$config = \Yaf\Registry::get('config')->database->toArray();
+			if(isset($config[$dbName]))
+			{
+				$this->db = Database::getInstance($config[$dbName], $dbName);
+			}else{
+				throw new \Exception('找不到数据库配置');
+			}
+		}
+	}
 }
